3268d8a60663b30176f6604daf1757fae9321e72
HougKai <32165244+HougKai5269@users.noreply.github.com>
2021-12-01 12:00:40 +0800

    Add files via upload

---
+++ week12/week12-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int a[10]={9,8,7,6,5,4,3,2,1,0};
+int main()
+{
+	for(int i=0;i<10;i++)
+	cout<<a[i]<<" ";
+	cout<<"\n";
+	for(int k=0;k<10;k++)
+	{
+	for(int i=0;i<10-1;i++)
+	{
+		if(a[i]>a[i+1])
+		{
+		int temp=a[i];
+		a[i]=a[i+1];
+		a[i+1]=temp;
+		}
+	}
+	for(int i=0;i<10;i++)
+    cout<<a[i]<<" ";
+	cout<<"\n";
+	}
+
+}
---
+++ week12/week12-3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int a[10]={9,8,7,6,5,4,3,2,1,0};
+int main()
+{
+	for(int i=0;i<10;i++)
+	cout<<a[i]<<" ";
+	cout<<"\n";
+	for(int i=0;i<10;i++)
+	{
+	for(int j=i+1;j<10;j++)
+	{
+		if(a[i]>a[j])
+		{
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		}
+	}
+	}
+    for(int i=0;i<10;i++)
+    cout<<a[i]<<" ";
+	cout<<"\n";
+}
+
