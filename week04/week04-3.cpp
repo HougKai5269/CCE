7318635d68990ede41189968e2797b2ca0f8afe3
HougKai <32165244+HougKai5269@users.noreply.github.com>
2021-12-01 10:14:01 +0800

    Add files via upload

---
+++ week04/week04-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=123 , b=10;
+
+    int ans= a/b;
+
+    float ans2=123.0/10.0;
+
+    printf("¾ã¼Æ°£¾ã¼Æ %d\n",ans);
+    printf("floatªº°£ªk %f\n",ans2);
+}
